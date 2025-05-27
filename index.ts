2d501836754a9495970c6b3969686f4a81db9143
KyAnh1126 <anh261104@gmail.com>
2025-05-27 15:40:23 +0700

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const port = process.env.PORT || 3000; 
 const app = express();
 
 app.use(express.json());
@@ -10,6 +13,6 @@ app.get('/', (req, res) => {
     res.json({ data: 'Welcome to my Page' });
 })
 
-app.listen(3000, 'localhost', () => {
-    console.log('server is running at port 3000');
+app.listen(port, () => {
+    console.log('server is running at port', port);
 })
